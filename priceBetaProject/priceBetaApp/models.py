a799e8fdc4143afb18ea04d2668bd0f433d6325f
Osooluwa <you@example.com>
2022-08-19 20:40:47 +0100

    I changed the max length from 200 to 250 under image url in models.py

--- priceBetaProject/priceBetaApp/models.py
+++ priceBetaProject/priceBetaApp/models.py
@@ -3,7 +3,7 @@ class Product(models.Model):
     name = models.CharField(max_length = 100)
     description = models.TextField()
     category = models.CharField(max_length = 100)
-    image_url = models.CharField(max_length = 200, null=True)
+    image_url = models.CharField(max_length = 250, null=True)
     purchase_link = models.CharField(max_length = 250, null=True)
     brand = models.CharField(max_length = 100)
     price = models.DecimalField(max_digits=100, decimal_places=2)
